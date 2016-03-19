854364bbc2620011992b90bd3739cb0010acd330
Han shenquan <1173956412@qq.com>
2016-03-20 00:03:51 +0800

    shuffle
    生成随机数组用Collections的shuffle。

---
+++ src/main/java/com/example/administrator/inputui_02_23/Shuffle.java
@@ -0,0 +1,33 @@
+package com.example.administrator.inputui_02_23;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Iterator;
+import java.util.List;
+
+/**
+ * Created by Administrator on 2016/3/19.
+ */
+public class Shuffle {
+    public static void shuffle(){
+        int[] x = {1,2,3,4,5,6,7,8,9};
+        List list = new ArrayList();
+        for(int i=0;i<x.length;i++){
+            System.out.print(x[i]+",");
+            list.add(x[i]);
+        }
+        System.out.println();
+
+        Collections.shuffle(list);
+
+        Iterator ite = list.iterator();
+        while(ite.hasNext()){
+            System.out.print(ite.next().toString()+",");
+        }
+
+    }
+    public static void main(String[] args){
+        shuffle();
+    }
+
+}
