f1973738d5908195b8fe9893168751377e61f499
Han shenquan <1173956412@qq.com>
2016-04-05 16:07:12 +0800

    UnitTest1
    UnitTest1

---
+++ src/main/java/com/example/administrator/inputui_02_23/UintTest.java
@@ -0,0 +1,11 @@
+package com.example.administrator.inputui_02_23;
+
+/**
+ * Created by Administrator on 2016/4/5.
+ */
+public class UintTest {
+
+    public static void main(String [] args){
+        System.out.print("OK");
+    }
+}
