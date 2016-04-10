402298e0098890ec65e62af73dc2d8febd98bb7d
Han shenquan <1173956412@qq.com>
2016-04-10 10:48:12 +0800

    v2

---
+++ src/main/java/com/example/administrator/inputui_02_23/MainSelectSort.java
@@ -0,0 +1,22 @@
+package com.example.administrator.inputui_02_23;
+
+/**
+ * Created by Administrator on 2016/4/10.
+ */
+public class MainSelectSort {
+
+    public static void selectSort(int arr[]){
+
+        for(int)
+
+    }
+    public static void main(String[] args) {
+        int a[]={5,4,9,8,7,6,0,1,3,2,};
+        int len=a.length;
+        selectSort(a);
+        for(int i=0;i<len;i++){
+            System.out.print(a[i]+" ");
+        }
+
+    }
+}
