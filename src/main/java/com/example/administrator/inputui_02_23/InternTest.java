fc82c97d3e3d75036f251c6c619bec3109b5e4cc
Han shenquan <1173956412@qq.com>
2016-04-11 11:05:32 +0800

    intern() string
    "java"字符串本来就存在，所以输出false

--- src/main/java/com/example/administrator/inputui_02_23/InternTest.java
+++ src/main/java/com/example/administrator/inputui_02_23/InternTest.java
@@ -6,12 +6,22 @@ package com.example.administrator.inputui_02_23;
 public class InternTest {
 
     public static void main(String[]args){
+        String str = new StringBuilder("ja").append("va").toString();
+        System.out.println(str.intern()==str);
+        
         String str1 = new StringBuilder("计算机").append("软件").toString();
         System.out.println(str1.intern()==str1);
 
-        String str3 = "java2";
+        String str3 = "java1";
         String str2 = new StringBuilder("ja").append("va1").toString();
+        System.out.println(str2);
         System.out.println(str2.intern()==str2);
+
+        String s4 = "abc";
+        String s6 = "abc";
+        String s5 = new String("abc");
+        System.out.println(s4==s5);
+        System.out.println(s4.equals(s5));
 
     }
 }
