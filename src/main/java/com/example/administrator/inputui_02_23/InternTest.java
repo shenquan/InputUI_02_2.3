125a9850363ca41256611675eabcc89750fce354
Han shenquan <1173956412@qq.com>
2016-04-11 10:50:56 +0800

    intern()

---
+++ src/main/java/com/example/administrator/inputui_02_23/InternTest.java
@@ -0,0 +1,17 @@
+package com.example.administrator.inputui_02_23;
+
+/**
+ * Created by Administrator on 2016/4/11.
+ */
+public class InternTest {
+
+    public static void main(String[]args){
+        String str1 = new StringBuilder("计算机").append("软件").toString();
+        System.out.println(str1.intern()==str1);
+
+        String str3 = "java2";
+        String str2 = new StringBuilder("ja").append("va1").toString();
+        System.out.println(str2.intern()==str2);
+
+    }
+}
